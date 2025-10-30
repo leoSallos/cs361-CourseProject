437097c06e7d137e05854de055184ad85a44dc61
Leo Sallos <sallosl@oregonstate.edu>
2025-10-30 15:11:38 -0700

    day selection is functional, no data loading yet

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var dayList = [];
 var today = new Date();
 const absToday = new Date();
 
 function buildTaskList(date){
+    // set header text
+    var header = document.getElementById("task-list-header");
+    var currDay = weekdays[date.getDay()];
+    var currDate = date.getDate();
+    var headerContent = "";
+    switch (currDate){
+        case 1: 
+        case 21:
+        case 31:
+            headerContent = currDay + ", " + currDate + "st";
+            break;
+        case 2: 
+        case 22:
+            headerContent = currDay + ", " + currDate + "nd";
+            break;
+        case 3: 
+        case 23:
+            headerContent = currDay + ", " + currDate + "rd";
+            break;
+        default:
+            headerContent = currDay + ", " + currDate + "th";
+    };
+    header.textContent = headerContent;
+}
 
+function refreshSelectedDay(){
+    buildTaskList(today);
 }
 
 function selectDay(day){
@@ -17,6 +45,26 @@ function selectDay(day){
     var dayID = "day-" + day;
     var toSelect = document.getElementById(dayID);
     toSelect.classList.add("selected");
+
+    // change date
+    var newDate = dayList[day];
+    if (newDate.month < today.getMonth()){
+        if (newDate.year == today.getFullYear()){
+            movePrevMonth(newDate.date);
+        } else {
+            moveNextMonth(newDate.date);
+        }
+    } else if (newDate.month > today.getMonth()){
+        if (newDate.year == today.getFullYear()){
+            moveNextMonth(newDate.date);
+        } else {
+            movePrevMonth(newDate.date);
+        }
+    } else {
+        today = new Date(newDate.year, newDate.month, newDate.date);
+        refreshSelectedDay();
+    }
+
 }
 
 function buildCalendarGrid(date){
@@ -33,6 +81,7 @@ function buildCalendarGrid(date){
     var idxDate = new Date(currTime - offsetMS);
 
     // create days
+    dayList = [];
     for (var i = 0; i < 42; i++){
         // create day num
         var dayNum = document.createElement('button');
@@ -48,7 +97,7 @@ function buildCalendarGrid(date){
         const dayID = i;
         dayNum.addEventListener("click", function(){selectDay(dayID);});
 
-        // create day div
+        // create day div 
         var day = document.createElement('div');
         day.classList.add("day-container");
         if (currMonth == absToday.getMonth() && idxDate.getFullYear() == absToday.getFullYear()){
@@ -68,6 +117,14 @@ function buildCalendarGrid(date){
         // append new elements
         day.appendChild(dayNum);
         container.appendChild(day);
+
+        // add to day list
+        const dayInfo = {
+            year: idxDate.getFullYear(), 
+            month: idxDate.getMonth(),
+            date: idxDate.getDate(),
+        };
+        dayList.push(dayInfo);
 
         // increment day
         var idxTime = idxDate.getTime();
@@ -92,42 +149,57 @@ function clearCalendar(){
     }
 }
 
-function movePrevMonth(){
+function movePrevMonth(date){
+    if (date){
+        var newDate = date;
+    } else {
+        var newDate = 0;
+    }
+
     // get prev month
     var prevMonth = today.getMonth() - 1;
     if (prevMonth < 0){
         prevMonth = 11;
         var year = today.getFullYear() - 1;
-        var prevMonthDate = new Date(year, prevMonth, 1);
+        var prevMonthDate = new Date(year, prevMonth, newDate);
     } else {
-        var prevMonthDate = new Date(today.getFullYear(), prevMonth, 1);
+        var prevMonthDate = new Date(today.getFullYear(), prevMonth, newDate);
     }
 
     // rebuildCalendar
     clearCalendar();
     buildCalendar(prevMonthDate);
     today = prevMonthDate;
+    refreshSelectedDay();
 }
 
-function moveNextMonth(){
+function moveNextMonth(date){
+    if (date){
+        var newDate = date;
+    } else {
+        var newDate = 0;
+    }
+
     // get prev month
     var nextMonth = today.getMonth() + 1;
     if (nextMonth >= 12){
         nextMonth = 0;
         var year = today.getFullYear() + 1;
-        var nextMonthDate = new Date(year, nextMonth, 1);
+        var nextMonthDate = new Date(year, nextMonth, newDate);
     } else {
-        var nextMonthDate = new Date(today.getFullYear(), nextMonth, 1);
+        var nextMonthDate = new Date(today.getFullYear(), nextMonth, newDate);
     }
 
     // rebuildCalendar
     clearCalendar();
     buildCalendar(nextMonthDate);
     today = nextMonthDate;
+    refreshSelectedDay();
 }
 
 function init(){
     buildCalendar(today);
+    buildTaskList(today);
 }
 
 // start of program
